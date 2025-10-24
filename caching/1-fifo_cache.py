e609c9bfe73ae7f2757a53a2d9fb2b121ee8d116
Nihad <nihadtaghiyev1@gmail.com>
2025-10-24 14:35:58 +0400

    task 1

--- caching/1-fifo_cache.py
+++ caching/1-fifo_cache.py
@@ -9,13 +9,12 @@ class FIFOCache(BaseCaching):
     def put(self, key, item):
         '''FIFO caching put function'''
         if key and item:
-            if len(self.cache_data) == BaseCaching.MAX_ITEMS:
+            self.cache_data[key] = item
+            
+            if len(self.cache_data) > BaseCaching.MAX_ITEMS:
                 first_key = list(self.cache_data.keys())[0]
                 print("DISCARD:", first_key)
                 del self.cache_data[first_key]
-                self.cache_data[key] = item
-            else:
-                self.cache_data[key] = item
 
     def get(self, key):
         '''Basic cache get function'''
