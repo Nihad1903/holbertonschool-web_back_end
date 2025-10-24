d2b37e16c072e9a9acb19f2a10abe00730feb23c
Nihad <nihadtaghiyev1@gmail.com>
2025-10-24 11:51:09 +0400

    task 0 corrected3

--- caching/0-basic_cache.py
+++ caching/0-basic_cache.py
@@ -3,12 +3,14 @@
 BaseCaching = __import__('base_caching').BaseCaching
 
 class BasicCache(BaseCaching):
-    
+    '''Basic cache'''
     def put(self, key, item):
+        '''Basic cache put function'''
         if key and item:
             self.cache_data[key] = item
         
     def get(self, key):
+        '''Basic cache get function'''
         val = self.cache_data.get(key)
         if val:
             return self.cache_data[key]
