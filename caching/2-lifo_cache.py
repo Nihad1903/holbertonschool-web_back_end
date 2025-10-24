90373da7f90555b25ba6ce73af5e7f9f46d7a22a
Nihad <nihadtaghiyev1@gmail.com>
2025-10-24 15:52:22 +0400

    task 2

---
+++ caching/2-lifo_cache.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+'''FIFO Cache'''
+
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class LIFOCache(BaseCaching):
+    '''LIFO cache class'''
+    def put(self, key, item):
+        if key and item:
+            self.cache_data[key] = item
+
+            if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+                first_key = list(self.cache_data.keys())[-1]
+                print("DISCARD:", first_key)
+                del self.cache_data[first_key]
+
+    def get(self, key):
+        '''LIFO cache get function'''
+        val = self.cache_data.get(key)
+        if val:
+            return self.cache_data[key]
+        else:
+            return None
