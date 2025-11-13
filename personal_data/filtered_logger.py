c30664bcafa17eca693290244344c301f5fda4d6
Nihad <nihadtaghiyev1@gmail.com>
2025-11-13 15:28:24 +0400

    task 0 corrected

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -3,6 +3,8 @@
 import re
 from typing import List
 
-def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
+def filter_datum(fields: List[str], redaction: str,
+                 message: str, separator: str) -> str:
     '''Filtered Datum Function'''
-    return re.sub(r'(' + '|'.join(fields) + r')=[^' + separator + r']*', lambda m: f"{m.group(1)}={redaction}", message)
+    return re.sub(r'(' + '|'.join(fields) + r')=[^' + separator + r']*', 
+                  lambda m: f"{m.group(1)}={redaction}", message)
