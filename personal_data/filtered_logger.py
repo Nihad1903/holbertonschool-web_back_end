a58f6690f92af7fb144f2cba48117058fd4748a1
Nihad <nihadtaghiyev1@gmail.com>
2025-11-13 15:34:44 +0400

    task 1 completed

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -1,11 +1,28 @@
 #!/usr/bin/env python3
-'''Filtered Logger File'''
+"""Filtered Logger File"""
 import re
+import logging
 from typing import List
-
 
 def filter_datum(fields: List[str], redaction: str,
                  message: str, separator: str) -> str:
-    '''Filtered Datum Function'''
+    """Filtered Datum Function"""
     return re.sub(r'(' + '|'.join(fields) + r')=[^' + separator + r']*',
                   lambda m: f"{m.group(1)}={redaction}", message)
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        record.msg = filter_datum(self.fields, self.REDACTION,
+                                  record.getMessage(), self.SEPARATOR)
+        return super().format(record)
