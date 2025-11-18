9d63c475bc2099797c82ffff4af53eaeed6bd682
Nihad <nihadtaghiyev1@gmail.com>
2025-11-18 12:44:16 +0400

    task 4 test

--- Basic_authentication/api/v1/auth/auth.py
+++ Basic_authentication/api/v1/auth/auth.py
@@ -11,7 +11,16 @@ class Auth():
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         '''Require Auth function
         '''
-        return False
+        if not path:
+            return True
+
+        elif not excluded_paths:
+            return True
+
+        elif path in excluded_paths or (path + '/') in excluded_paths:
+            return False
+
+        return True
 
     def authorization_header(self, request=None) -> str:
         '''Authorization Header'''
