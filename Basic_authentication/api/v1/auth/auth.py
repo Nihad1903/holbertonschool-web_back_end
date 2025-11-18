5f07033052b382ea09a0f64af1693574f0f821a0
Nihad <nihadtaghiyev1@gmail.com>
2025-11-18 12:31:23 +0400

    task 3

---
+++ Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+'''Auth class module
+'''
+from flask import request
+from typing import List, TypeVar
+
+class Auth():
+    '''Auth Class'''
+
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        '''Require Auth function
+        '''
+        return False
+
+    
+    def authorization_header(self, request=None) -> str:
+        '''Authorization Header'''
+        return None
+
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        '''Current User'''
+        return None
