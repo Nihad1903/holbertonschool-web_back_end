986ef28c4f299cd1069866a99945c98e9aae7ead
Nihad <nihadtaghiyev1@gmail.com>
2025-11-18 12:32:42 +0400

    task 3 corrected

--- Basic_authentication/api/v1/auth/auth.py
+++ Basic_authentication/api/v1/auth/auth.py
@@ -4,6 +4,7 @@
 from flask import request
 from typing import List, TypeVar
 
+
 class Auth():
     '''Auth Class'''
 
@@ -11,12 +12,10 @@ class Auth():
         '''Require Auth function
         '''
         return False
-
     
     def authorization_header(self, request=None) -> str:
         '''Authorization Header'''
         return None
-
 
     def current_user(self, request=None) -> TypeVar('User'):
         '''Current User'''
