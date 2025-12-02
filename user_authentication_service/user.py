845550782e73abd0c673329ed5632e0d090105c3
Nihad <nihadtaghiyev1@gmail.com>
2025-12-02 11:44:12 +0400

    task 0 corrected

--- user_authentication_service/user.py
+++ user_authentication_service/user.py
@@ -2,18 +2,18 @@
 """
 User Model File
 """
-
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.orm import declarative_base
 
 Base = declarative_base()
+
 
 class User(Base):
     '''User Model'''
     __tablename__ = 'users'
     
     id = Column(Integer, primary_key = True)
-    email = Column(String, primary_key = True)
-    hashed_password = Column(String, primary_key = True)
-    session_id = Column(String, primary_key = True)
-    reset_token = Column(String, primary_key = True)    
+    email = Column(String(250), primary_key = True)
+    hashed_password = Column(String(250), primary_key = True)
+    session_id = Column(String(250), primary_key = True)
+    reset_token = Column(String(250), primary_key = True)    
