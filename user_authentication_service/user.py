8322e6cb185c69e7a48d34f3c8117a4e1120c061
Nihad <nihadtaghiyev1@gmail.com>
2025-12-02 11:46:13 +0400

    task 0 corrected

--- user_authentication_service/user.py
+++ user_authentication_service/user.py
@@ -11,9 +11,9 @@ Base = declarative_base()
 class User(Base):
     '''User Model'''
     __tablename__ = 'users'
-    
+
     id = Column(Integer, primary_key=True)
     email = Column(String(250), primary_key=True)
     hashed_password = Column(String(250), primary_key=True)
     session_id = Column(String(250), primary_key=True)
-    reset_token = Column(String(250), primary_key=True)    
+    reset_token = Column(String(250), primary_key=True)
