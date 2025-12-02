a3d02da778d540644990a40cb7a7490d9ce8880a
Nihad <nihadtaghiyev1@gmail.com>
2025-12-02 11:45:12 +0400

    task 0 corrected

--- user_authentication_service/user.py
+++ user_authentication_service/user.py
@@ -12,8 +12,8 @@ class User(Base):
     '''User Model'''
     __tablename__ = 'users'
     
-    id = Column(Integer, primary_key = True)
-    email = Column(String(250), primary_key = True)
-    hashed_password = Column(String(250), primary_key = True)
-    session_id = Column(String(250), primary_key = True)
-    reset_token = Column(String(250), primary_key = True)    
+    id = Column(Integer, primary_key=True)
+    email = Column(String(250), primary_key=True)
+    hashed_password = Column(String(250), primary_key=True)
+    session_id = Column(String(250), primary_key=True)
+    reset_token = Column(String(250), primary_key=True)    
