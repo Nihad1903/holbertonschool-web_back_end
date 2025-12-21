6d3d6d2dc03da4eee2a443177fb7903de5389004
Nihad <nihadtaghiyev1@gmail.com>
2025-12-21 11:22:50 +0400

    redis task0 corrected

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -2,6 +2,7 @@
 '''Exercise File'''
 import redis
 import uuid
+from typing import Union
 
 class Cache():
     '''Redis Cache Class'''
@@ -9,7 +10,7 @@ class Cache():
         self._redis = redis.Redis()
         self._redis.flushdb()
 
-    def store(self, data) -> str:
+    def store(self, data: Union[str, bytes, int, float]) -> str:
         '''Redis Cache store method'''
         key = str(uuid.uuid4())
         self._redis.set(key, data)
